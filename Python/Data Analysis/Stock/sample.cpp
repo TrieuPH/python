3357ccdfc339ee48568659319e9d06006d2e93b6
TrieuPH <68679466+TrieuPH@users.noreply.github.com>
2023-05-24 20:13:56 +0700

    update files

---
+++ Python/Data Analysis/Stock/sample.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main()
+{   
+    // Double value of x
+    double x = 3.14;
+    cout << "x = " << x << endl;
+    // Read csv file
+
+}
